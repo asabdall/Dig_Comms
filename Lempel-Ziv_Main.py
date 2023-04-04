bd27ef57221e635f5aed668b2d25e7696cccd55e
Abdalla, Ahmed S <asabdall@iu.edu>
2023-04-04 19:13:50 -0400

    h

---
+++ Lempel-Ziv_Main.py
@@ -0,0 +1,8 @@
+bit_sequence = [1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1,
+                0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
+codebook = {"Numerical Positions": [1,2],
+            "Subsequences": [0,1],
+            "Numerical Representations": ["NA","NA"],
+            "Binary Encoded Blocks": ["NA","NA"]
+            }
+for i in range(0,bit_sequence):
