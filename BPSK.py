2766faf40ef3714706f2a9a3da7ec3aa01a1a5f7
Abdalla, Ahmed S <asabdall@iu.edu>
2023-04-16 03:59:15 -0400

    Working on  BPSK Modulator

--- BPSK.py
+++ BPSK.py
@@ -1,16 +1,39 @@
 import numpy as np
+import pandas as pd
+import plotly.express as px
 import math
 
 
 def binary_phase_shift_keying_modulator(bitstream, bit_rate, carrier_frequency):
     total_time = len(bitstream) / bit_rate
-    seconds = np.linspace(0, total_time, 10 * bit_rate)
+    seconds = np.linspace(0, total_time, len(bitstream) * 100)
     sine_wave = np.sin(2 * math.pi * carrier_frequency * seconds)
     cosine_wave = np.cos(2 * math.pi * carrier_frequency * seconds)
-    modulated_wave=[0]*len(seconds)
-    for i in range(0,len(seconds)):
+    modulated_wave = [0] * len(seconds)
+    window_size = int(len(seconds) / len(bitstream))
+    for i in range(0, len(bitstream)):
+        if "1" in bitstream[i]:
+            modulated_wave[i * window_size:(i + 1) * window_size] = sine_wave[i * window_size:(i + 1) * window_size]
+        else:
+            modulated_wave[i * window_size:(i + 1) * window_size] = cosine_wave[i * window_size:(i + 1) * window_size]
+    modulated_wave = np.array(modulated_wave)
+    return modulated_wave, cosine_wave, window_size,seconds
 
-    print('hi')
+
+def binary_phase_shift_keying_demodulator(received_message, reference_signal, window_size,time):
+    demodulated_signal = np.multiply(received_message, reference_signal)
+    bit_count = int(demodulated_signal.size / window_size)
+    received_bits = ""
+    for i in range(0, bit_count):
+        bit_signal = demodulated_signal[i * window_size:(i + 1) * window_size]
+        signal_integral = np.trapz(bit_signal)
+        if signal_integral > .25:
+            received_bits += "1"
+        else:
+            received_bits += "0"
+    return received_bits
 
 
-binary_phase_shift_keying_modulator("1010101010", 100, 2000)
+message_signal, reference_signal, window_size,time = binary_phase_shift_keying_modulator("1010101010", 10, 20)
+received_message=binary_phase_shift_keying_demodulator(message_signal, reference_signal, window_size,time)
+print('hi')
