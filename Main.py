d097fbd4bb856bd2a61fe87b6a59fdfd7abc7a86
Abdalla, Ahmed S <asabdall@iu.edu>
2023-04-18 00:04:33 -0400

    Commented and cleaned up code, as well as added AWGN Channel

--- Main.py
+++ Main.py
@@ -3,37 +3,31 @@ import Random_Bit_Generator as Rand
 import BPSK
 import AWGN_Channel as AWGN
 
-# Define the size of the message to be sent
-message_size = 1000
 
-# Generate a random message of given size
-random_message = Rand.random_bit_generator(message_size)
+def simulate_communication_system(message_size, bits_per_second, carrier_frequency, signal_to_noise_ratio_db):
+    # Generate a random message of given size
+    random_message = Rand.random_bit_generator(message_size)
 
-# Define the number of bits per second to be used in BPSK modulation
-bits_per_second = 100
+    # Modulate the message using binary phase shift keying (BPSK) modulation
+    modulated_message, base_func_1, base_func_0 = BPSK.binary_phase_shift_keying_modulator(random_message,
+                                                                                           bits_per_second
+                                                                                           , carrier_frequency)
 
-# Define the carrier frequency to be used in BPSK modulation
-carrier_frequency = 2000
+    # Determine the window size for demodulation
+    window_size = int(modulated_message.size / len(random_message))
 
-# Modulate the message using binary phase shift keying (BPSK) modulation
-modulated_message, base_func_1, base_func_0 = BPSK.binary_phase_shift_keying_modulator(random_message, bits_per_second
-                                                                                       , carrier_frequency)
+    # Simulate the effect of the AWGN channel on the modulated message
+    received_message = AWGN.additive_white_gaussian_noise_channel(modulated_message, signal_to_noise_ratio_db)
 
-# Determine the window size for demodulation
-window_size = int(modulated_message.size / len(random_message))
+    # Demodulate the message using BPSK demodulation and the base functions
+    demodulated_message = BPSK.binary_phase_shift_keying_demodulator(received_message, base_func_1, base_func_0,
+                                                                     window_size)
 
-# Define the signal-to-noise ratio in DB for the AWGN channel
-signal_to_noise_ratio_db = -0
+    # Count the number of errors between the original and demodulated messages
+    errors = sum(1 for a, b in zip(random_message, demodulated_message) if a != b)
 
-# Simulate the effect of the AWGN channel on the modulated message
-received_message = AWGN.additive_white_gaussian_noise_channel(modulated_message, signal_to_noise_ratio_db)
+    # Print the number of errors between the original and demodulated messages
+    print(f"Bit Error Rate: {errors / message_size}")
 
-# Demodulate the message using BPSK demodulation and the base functions
-demodulated_message = BPSK.binary_phase_shift_keying_demodulator(received_message, base_func_1, base_func_0,
-                                                                 window_size)
 
-# Count the number of errors between the original and demodulated messages
-errors = sum(1 for a, b in zip(random_message, demodulated_message) if a != b)
-
-# Print the number of errors between the original and demodulated messages
-print(f"Number of errors: {errors}")
+simulate_communication_system(100, 100, 2000, -10)
